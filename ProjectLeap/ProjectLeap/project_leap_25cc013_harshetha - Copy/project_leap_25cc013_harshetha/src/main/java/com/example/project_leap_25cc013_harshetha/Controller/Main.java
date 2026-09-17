25cd7a2fab6322c817f15f507443e2c15748d585
harshethad2025cce-sketch <harshetha.d2025cce@sece.ac.in>
2026-09-17 15:13:58 +0530

    Add files via upload

---
+++ ProjectLeap/ProjectLeap/project_leap_25cc013_harshetha - Copy/project_leap_25cc013_harshetha/src/main/java/com/example/project_leap_25cc013_harshetha/Controller/Main.java
@@ -0,0 +1,8 @@
+package com.example.project_leap_25cc13_harshetha.Controller;
+
+/*public class Main {
+    public static void main(String [] args){
+        WebController wc=new WebController();
+        System.out.println(wc.calculate());
+    }
+}*/
---
+++ ProjectLeap/ProjectLeap/project_leap_25cc013_harshetha - Copy/project_leap_25cc013_harshetha/src/test/java/com/example/project_leap_25cc013_harshetha/ProjectLeap25cc013harshethaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.project_leap_25cc059_swathika;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectLeap25cc059SwathikaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
